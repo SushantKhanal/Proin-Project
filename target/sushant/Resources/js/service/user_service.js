331c8d5dc8398a38fef89cd76bc6d2b810420441
SushantKhanal <dreamerking114@gmail.com>
2018-02-26 11:37:18 +0545

    user-service.js created, user_controller.js creating

---
+++ target/sushant/Resources/js/service/user_service.js
@@ -0,0 +1,9 @@
+(function(){
+    'use strict';
+    angular
+        .module("myApp")
+        .factory('UserService', ['$http', '$q', function($http, $q){
+            var REST_SERVICE_URI = '<a class="vglnk" href="http://localhost:8080/proinapp/user/" rel="nofollow"></a>';
+
+        }
+})();
